2a1451082b66b190eef06a5145e7b11f29958738
amor9121 <amor9121@gmail.com>
2022-08-09 17:07:41 +0800

    商品功能 - 實作查詢商品

---
+++ src/main/java/com/romafu/springbootmall/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.romafu.springbootmall.controller;
+
+import com.romafu.springbootmall.model.Product;
+import com.romafu.springbootmall.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products/{productid}")
+    public ResponseEntity<Product> getProduct(@PathVariable Integer productid){
+        Product product = productService.getProductById(productid);
+        if(product != null){
+            return ResponseEntity.status(HttpStatus.OK).body(product);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+    }
+
+
+}
---
+++ src/main/java/com/romafu/springbootmall/service/ProductService.java
@@ -0,0 +1,7 @@
+package com.romafu.springbootmall.service;
+
+import com.romafu.springbootmall.model.Product;
+
+public interface ProductService {
+    Product getProductById(Integer productId);
+}
