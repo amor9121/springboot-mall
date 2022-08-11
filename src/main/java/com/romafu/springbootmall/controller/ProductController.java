a65b5a81f392c4ed113ec704702a284a889c53bc
amor9121 <amor9121@gmail.com>
2022-08-11 15:36:49 +0800

    商品功能 - 實作修改商品

--- src/main/java/com/romafu/springbootmall/controller/ProductController.java
+++ src/main/java/com/romafu/springbootmall/controller/ProductController.java
@@ -16,9 +16,9 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/products/{productid}")
-    public ResponseEntity<Product> getProduct(@PathVariable Integer productid){
-        Product product = productService.getProductById(productid);
+    @GetMapping("/products/{productId}")
+    public ResponseEntity<Product> getProduct(@PathVariable Integer productId){
+        Product product = productService.getProductById(productId);
         if(product != null){
             return ResponseEntity.status(HttpStatus.OK).body(product);
         }else{
@@ -36,5 +36,23 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
 
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest){
+
+        //檢查product是否存在
+        Product product = productService.getProductById(productId);
+
+        if(product == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        //修改商品數據
+        productService.updateProduct(productId, productRequest);
+
+        Product updateProduct = productService.getProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
+    }
 
 }
--- src/main/java/com/romafu/springbootmall/service/ProductService.java
+++ src/main/java/com/romafu/springbootmall/service/ProductService.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.cassandra.CassandraProperties;
 
 public interface ProductService {
     Product getProductById(Integer productId);
-
     Integer createProduct(ProductRequest productRequest);
+    void updateProduct(Integer productId, ProductRequest productRequest);
 }
