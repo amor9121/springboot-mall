c9c908184731993abbd414501a2376c569915a5c
amor9121 <amor9121@gmail.com>
2022-08-18 16:37:47 +0800

    帳號功能 - 檢查註冊的email, 隱藏返回的密碼

--- src/main/java/com/romafu/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/romafu/springbootmall/service/impl/UserServiceImpl.java
@@ -4,8 +4,12 @@ import com.romafu.springbootmall.dao.UserDao;
 import com.romafu.springbootmall.dto.UserRegisterRequest;
 import com.romafu.springbootmall.model.User;
 import com.romafu.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
@@ -13,13 +17,25 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserDao userDao;
 
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest){
+
+        //檢查註冊的email
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if(user != null){
+            log.warn("該 email {} 已經被註冊", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        //創建帳號
         return userDao.createUser(userRegisterRequest);
     }
 
     @Override
     public User getUserById(Integer userId) {
+
         return userDao.getUserById(userId);
     }
 
